bd1f4a08dc1a3ec211b06ab50451bcfdf4f5daee
purepennons <y80811@gmail.com>
2021-07-24 21:13:13 +0800

    feat: enahnce the `QuoteRow` component with background flash

--- src/order-book/components/OrderTable/QuoteRow.jsx
+++ src/order-book/components/OrderTable/QuoteRow.jsx
@@ -1,5 +1,8 @@
+import { Component } from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
+
+import FlashBackground from '../FlashBackground'
 
 const quoteRowTheme = {
     buy: {
@@ -16,16 +19,37 @@ const quoteRowTheme = {
     },
 }
 
-const QuoteRow = styled(function QuoteRow(props) {
-    const { className, price, size, total } = props
-    return (
-        <tr className={className}>
-            <td className="price">{price}</td>
-            <td>{size}</td>
-            <td>{total}</td>
-        </tr>
-    )
-}).attrs((props) => ({ modeTheme: quoteRowTheme[props.mode] }))`
+const QuoteRow = styled(
+    class QuoteRow extends Component {
+        componentDidUpdate(prevProps) {
+            const { shouldFlashWhenPropsChange, triggerFlash } = this.props
+            if (shouldFlashWhenPropsChange(this.props, prevProps)) {
+                triggerFlash()
+            }
+        }
+
+        render() {
+            const {
+                className,
+                price,
+                size,
+                total,
+                targetRef,
+                flashAnimationClassName,
+            } = this.props
+            return (
+                <tr
+                    ref={targetRef}
+                    className={`${className} ${flashAnimationClassName}`}
+                >
+                    <td className="price">{price}</td>
+                    <td>{size}</td>
+                    <td>{total}</td>
+                </tr>
+            )
+        }
+    }
+).attrs((props) => ({ modeTheme: quoteRowTheme[props.mode] }))`
     font-size: ${(props) => props.theme.sizes.m};
     color: ${(props) => props.theme.colors.text};
     cursor: pointer;
@@ -52,11 +76,33 @@ const QuoteRow = styled(function QuoteRow(props) {
     }
 `
 
-QuoteRow.propTypes = {
-    mode: PropTypes.oneOf(['sell', 'buy']),
-}
-QuoteRow.defaultProps = {
-    mode: 'sell',
+function QuoteRowWithFlash(props) {
+    return (
+        <FlashBackground enable>
+            {({ targetRef, flashAnimationClassName, triggerFlash }) => (
+                <QuoteRow
+                    targetRef={targetRef}
+                    flashAnimationClassName={flashAnimationClassName}
+                    triggerFlash={triggerFlash}
+                    {...props}
+                />
+            )}
+        </FlashBackground>
+    )
 }
 
-export default QuoteRow
+QuoteRowWithFlash.propTypes = {
+    mode: PropTypes.oneOf(['sell', 'buy']).isRequired,
+    id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+    price: PropTypes.number.isRequired,
+    size: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    flashColor: PropTypes.string.isRequired,
+    shouldFlashWhenPropsChange: PropTypes.func,
+}
+
+QuoteRowWithFlash.defaultProps = {
+    shouldFlashWhenPropsChange: () => false,
+}
+
+export default QuoteRowWithFlash
